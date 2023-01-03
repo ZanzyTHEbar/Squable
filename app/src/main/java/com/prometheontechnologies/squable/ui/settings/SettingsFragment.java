045f4e89d6054c739e37b4fddc287056668bc00c
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-03 20:49:16 +0000

    update color text
    Signed-off-by: ZanzyTHEbar <pyr0ndet0s97@gmail.com>

--- app/src/main/java/com/prometheontechnologies/squable/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/prometheontechnologies/squable/ui/settings/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.prometheontechnologies.squable.ui.gallery;
+package com.prometheontechnologies.squable.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.prometheontechnologies.squable.databinding.FragmentGalleryBinding;
+import com.prometheontechnologies.squable.databinding.FragmentSettingsBinding;
 
-public class GalleryFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textSettings;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
