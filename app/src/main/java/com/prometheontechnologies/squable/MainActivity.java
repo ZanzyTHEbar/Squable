d147d44d0015d4c60b1ea02e545186061d76d0fb
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-03 20:19:12 +0000

    update color text
    Signed-off-by: ZanzyTHEbar <pyr0ndet0s97@gmail.com>

--- app/src/main/java/com/prometheontechnologies/squable/MainActivity.java
+++ app/src/main/java/com/prometheontechnologies/squable/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
          menu should be considered as top level destinations.
         */
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_me, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_me, R.id.nav_settings)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
